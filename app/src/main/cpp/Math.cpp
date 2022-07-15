0207f7ab7a27e02693b614c702ffb81e98060f56
aweirdtrashcan <diegodrp01@gmail.com>
2022-07-15 01:43:08 -0300

    :p

--- app/src/main/cpp/Math.cpp
+++ app/src/main/cpp/Math.cpp
@@ -15,6 +15,10 @@ Java_com_aweirdtrashcan_ndktutorial_Math_add(
 
 extern "C"
 JNIEXPORT jfloat JNICALL
-Java_com_aweirdtrashcan_ndktutorial_Math_someCrazyMath(JNIEnv *env, jobject thiz, jint a, jint b) {
+Java_com_aweirdtrashcan_ndktutorial_Math_someCrazyMath(
+        JNIEnv *env,
+        jobject thiz,
+        jint a,
+        jint b) {
     return static_cast<jfloat>(a * a * a * (b / a * 88));
 }
